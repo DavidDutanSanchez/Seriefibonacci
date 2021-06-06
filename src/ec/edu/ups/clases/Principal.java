d8953ff438545ef9c84e45de83e12c2511d1ab56
surfa <surfa@SurfaceBook2>
2021-06-06 03:21:06 -0400

    primer comit

---
+++ src/ec/edu/ups/clases/Principal.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.clases;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author surfa
+ */
+public class Principal {
+     public static void main(String[] args){
+ 
+        Scanner sc = new Scanner(System.in);
+        int numero,fibo1,fibo2,i;
+  
+        do{
+            System.out.print("Ingresa cantidad a desplegar: ");
+            numero = sc.nextInt();
+        }while(numero<=1);
+  
+        System.out.println("Los " + numero + " primeros términos de la serie de Fibonacci son:");                 
+
+        fibo1=1;
+        fibo2=1;
+
+        System.out.print(fibo1 + " ");
+        for(i=2;i<=numero;i++){
+            System.out.print(fibo2 + " ");
+            fibo2 = fibo1 + fibo2;
+            fibo1 = fibo2 - fibo1;
+        }
+        System.out.println();
+    }
+}
